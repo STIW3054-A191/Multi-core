68f2e26e4572eba3d909ddae718054ede11c9b68
liviniesh nambiar <47709908+liviniesh@users.noreply.github.com>
2020-01-07 20:26:12 +0800

    Add files via upload

---
+++ src/main/java/Directory.java
@@ -0,0 +1,10 @@
+public class Directory {
+
+
+    //Create Directory
+    // Set Directory when Location is Needed
+    // Get Directory to Read Back Java Jar
+    public void mkDir(){}
+    public void setDir(){}
+    public void gtDir(){}
+}
