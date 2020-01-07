05f744c68b95a251256f769653b6ef5b16995314
liviniesh nambiar <47709908+liviniesh@users.noreply.github.com>
2020-01-07 23:42:32 +0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,26 @@
-public class Main {
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-    //Main Executor
+public class Main {
+    public static void main(String []args) throws InterruptedException, IOException {
+
+        int core = Runtime.getRuntime().availableProcessors();
+        ExecutorService service = Executors.newFixedThreadPool(core);
+//     Respository r1 = new Respository();
+//     service.submit(r1);
+
+//     Compiler cp = new Compiler();
+//        cp.compile();
+
+        ControlUnit r = new ControlUnit();
+         r.runProgram();
+r.metricsca();
+
+
+    }
+
+
+
 }
+
