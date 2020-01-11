0425efe803d51564a573258acc940b35a2f0244b
prayat15 <45549060+prayat15@users.noreply.github.com>
2020-01-11 20:36:23 +0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
         int core = Runtime.getRuntime().availableProcessors();
         ExecutorService service = Executors.newFixedThreadPool(core);
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please set Directory : ");
+        System.out.println("Please set Directory :  ");
         String gtpath = scan.nextLine();
 
         CountDownLatch buildFolder = new CountDownLatch(1);
@@ -18,7 +18,7 @@ public class Main {
         buildFol.start();
         buildFolder.await();
 
-        System.out.println("Building Download Folder...");
+        System.out.println("Building Download Folder.... ");
         System.out.println("Cloning...");
         Grabber co = new Grabber();
 
@@ -27,16 +27,16 @@ public class Main {
         Thread threadCloneRepo = new Thread(new Respository(repoCount,MainDirectory.getMainPath()));
         threadCloneRepo.start();
         repoCount.await();
-        System.out.println("Cloning Completed....");
+        System.out.println("Cloning Completed.... ");
         System.out.println("Saved files into : "+MainDirectory.getMainPath());
 
 
-        System.out.println("Compiling process....");
+        System.out.println("Compiling process.... ");
         CountDownLatch comCount = new CountDownLatch(1);
         Thread comThread = new Thread(new Compiler(comCount,MainDirectory.getMainPath()));
         comThread.start();
         comCount.await();
-        System.out.println("Compiling is Done....");
+        System.out.println("Compiling is Done.... ");
 
         CountDownLatch runLatch = new CountDownLatch(1);
          ControlUnit r = new ControlUnit(runLatch,MainDirectory.getMainPath());
@@ -44,9 +44,9 @@ public class Main {
          runJar.start();
          runLatch.await();
 
-         System.out.println("Printing CKJM metrics & Exporting to Excel...");
+         System.out.println("Printing CKJM metrics & Exporting to Excel... ");
          r.metricsca();
-         System.out.println("Done Extracting to Excel !");
+         System.out.println("Done Extracting to Excel ! ");
          ProcessTime.end();
 
 
